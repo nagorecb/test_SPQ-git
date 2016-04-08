4ec16eb65a34a6eec7d0b76caa3ffee43b71371c
dipina <dipina@gmail.com>
2016-04-08 10:03:05 +0200

    added a semicolon to Barbarian.java

--- GameAdventureStrategy/src/game/Barbarian.java
+++ GameAdventureStrategy/src/game/Barbarian.java
@@ -10,6 +10,6 @@ public class Barbarian extends Character {
 	public void display() {
 	// TODO Auto-generated method stub
 		System.out.println("You are a strong, hulky barbarian....");
-		System.out.println("Modified from A");
+		System.out.println("Modified from A");;
 	}
 }
