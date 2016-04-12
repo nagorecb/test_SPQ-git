5d75ff63820dde975409d5265cc3c0de0a7722cc
dipina <dipina@gmail.com>
2016-04-12 06:48:26 +0200

    Adding Elfo character #PGP-1 add work 1h Development

---
+++ GameAdventureStrategy/src/game/Elfo.java
@@ -0,0 +1,15 @@
+package game;
+
+public class Elfo extends Character {
+
+	public Elfo(WeaponBehaviour w) {
+		super(w);
+	}
+	
+	@Override
+	public void display() {
+	// TODO Auto-generated method stub
+		System.out.println("You are a strong, hulky barbarian....");
+		System.out.println("Modified from A!!!!!!");;
+	}
+}
