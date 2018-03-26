41a16b64343626d17afd2f41713cfcedfdd83dd8
dipina <dipina@gmail.com>
2018-03-26 09:13:14 +0200

    I have worked on the UI of Barbarian.java #DIS18-5 Done

--- GameAdventureStrategyMaven/src/main/java/game/Barbarian.java
+++ GameAdventureStrategyMaven/src/main/java/game/Barbarian.java
@@ -3,7 +3,7 @@ package game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Just created the GUIs
+// Just created the GUI
 public class Barbarian extends Character {
 	final Logger logger = LoggerFactory.getLogger(Barbarian.class);
 	
